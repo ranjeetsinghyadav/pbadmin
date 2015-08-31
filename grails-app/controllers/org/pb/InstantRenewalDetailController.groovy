2218aa3af7ed8dab6ad5cd31a6d64627dc5c5da4
Ranjeet Singh Yadav <ranjeets@policybazaar.com>
2015-08-31 19:27:57 +0530

    Setting default port to 8090

--- grails-app/controllers/org/pb/InstantRenewalDetailController.groovy
+++ grails-app/controllers/org/pb/InstantRenewalDetailController.groovy
@@ -8,97 +8,109 @@ import grails.transaction.Transactional
 @Transactional(readOnly = true)
 class InstantRenewalDetailController {
 
-    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+	static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
 
-    def index(Integer max) {
-        params.max = Math.min(max ?: 10, 100)
-        respond InstantRenewalDetail.list(params), model:[instantRenewalDetailInstanceCount: InstantRenewalDetail.count()]
-    }
+	def index(Integer max) {
+		params.max = Math.min(max ?: 10, 100)
+		respond InstantRenewalDetail.list(params), model:[instantRenewalDetailInstanceCount: InstantRenewalDetail.count()]
+	}
 
-    def show(InstantRenewalDetail instantRenewalDetailInstance) {
-        respond instantRenewalDetailInstance
-    }
+	def show(InstantRenewalDetail instantRenewalDetailInstance) {
+		respond instantRenewalDetailInstance
+	}
 
-    def create() {
-        respond new InstantRenewalDetail(params)
-    }
+	def create() {
+		respond new InstantRenewalDetail(params)
+	}
 
-    @Transactional
-    def save(InstantRenewalDetail instantRenewalDetailInstance) {
-        if (instantRenewalDetailInstance == null) {
-            notFound()
-            return
-        }
+	@Transactional
+	def save(InstantRenewalDetail instantRenewalDetailInstance) {
+		if (instantRenewalDetailInstance == null) {
+			notFound()
+			return
+		}
 
-        if (instantRenewalDetailInstance.hasErrors()) {
-            respond instantRenewalDetailInstance.errors, view:'create'
-            return
-        }
+		if (instantRenewalDetailInstance.hasErrors()) {
+			respond instantRenewalDetailInstance.errors, view:'create'
+			return
+		}
 
-        instantRenewalDetailInstance.save flush:true
+		instantRenewalDetailInstance.save flush:true
 
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.created.message', args: [message(code: 'instantRenewalDetail.label', default: 'InstantRenewalDetail'), instantRenewalDetailInstance.id])
-                redirect instantRenewalDetailInstance
-            }
-            '*' { respond instantRenewalDetailInstance, [status: CREATED] }
-        }
-    }
+		request.withFormat {
+			form multipartForm {
+				flash.message = message(code: 'default.created.message', args: [
+					message(code: 'instantRenewalDetail.label', default: 'InstantRenewalDetail'),
+					instantRenewalDetailInstance.id
+				])
+				redirect instantRenewalDetailInstance
+			}
+			'*' { respond instantRenewalDetailInstance, [status: CREATED] }
+		}
+	}
 
-    def edit(InstantRenewalDetail instantRenewalDetailInstance) {
-        respond instantRenewalDetailInstance
-    }
+	def edit(InstantRenewalDetail instantRenewalDetailInstance) {
+		respond instantRenewalDetailInstance
+	}
 
-    @Transactional
-    def update(InstantRenewalDetail instantRenewalDetailInstance) {
-        if (instantRenewalDetailInstance == null) {
-            notFound()
-            return
-        }
+	@Transactional
+	def update(InstantRenewalDetail instantRenewalDetailInstance) {
+		if (instantRenewalDetailInstance == null) {
+			notFound()
+			return
+		}
 
-        if (instantRenewalDetailInstance.hasErrors()) {
-            respond instantRenewalDetailInstance.errors, view:'edit'
-            return
-        }
+		if (instantRenewalDetailInstance.hasErrors()) {
+			respond instantRenewalDetailInstance.errors, view:'edit'
+			return
+		}
 
-        instantRenewalDetailInstance.save flush:true
+		instantRenewalDetailInstance.save flush:true
 
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.updated.message', args: [message(code: 'InstantRenewalDetail.label', default: 'InstantRenewalDetail'), instantRenewalDetailInstance.id])
-                redirect instantRenewalDetailInstance
-            }
-            '*'{ respond instantRenewalDetailInstance, [status: OK] }
-        }
-    }
+		request.withFormat {
+			form multipartForm {
+				flash.message = message(code: 'default.updated.message', args: [
+					message(code: 'InstantRenewalDetail.label', default: 'InstantRenewalDetail'),
+					instantRenewalDetailInstance.id
+				])
+				redirect instantRenewalDetailInstance
+			}
+			'*'{ respond instantRenewalDetailInstance, [status: OK] }
+		}
+	}
 
-    @Transactional
-    def delete(InstantRenewalDetail instantRenewalDetailInstance) {
+	@Transactional
+	def delete(InstantRenewalDetail instantRenewalDetailInstance) {
 
-        if (instantRenewalDetailInstance == null) {
-            notFound()
-            return
-        }
+		if (instantRenewalDetailInstance == null) {
+			notFound()
+			return
+		}
 
-        instantRenewalDetailInstance.delete flush:true
+		instantRenewalDetailInstance.delete flush:true
 
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.deleted.message', args: [message(code: 'InstantRenewalDetail.label', default: 'InstantRenewalDetail'), instantRenewalDetailInstance.id])
-                redirect action:"index", method:"GET"
-            }
-            '*'{ render status: NO_CONTENT }
-        }
-    }
+		request.withFormat {
+			form multipartForm {
+				flash.message = message(code: 'default.deleted.message', args: [
+					message(code: 'InstantRenewalDetail.label', default: 'InstantRenewalDetail'),
+					instantRenewalDetailInstance.id
+				])
+				redirect action:"index", method:"GET"
+			}
+			'*'{ render status: NO_CONTENT }
+		}
+	}
 
-    protected void notFound() {
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.not.found.message', args: [message(code: 'instantRenewalDetail.label', default: 'InstantRenewalDetail'), params.id])
-                redirect action: "index", method: "GET"
-            }
-            '*'{ render status: NOT_FOUND }
-        }
-    }
+	protected void notFound() {
+		request.withFormat {
+			form multipartForm {
+				flash.message = message(code: 'default.not.found.message', args: [
+					message(code: 'instantRenewalDetail.label', default: 'InstantRenewalDetail'),
+					params.id
+				])
+				redirect action: "index", method: "GET"
+			}
+			'*'{ render status: NOT_FOUND }
+		}
+	}
 }
